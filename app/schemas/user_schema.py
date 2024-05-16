f2bfb0d2ae5c485eabc8aa0b7bf690a1b9328e55
ayoubm <ayoubmori33@gmail.com>
2024-05-16 03:14:41 +0100

    add restapi control program v2

--- app/schemas/user_schema.py
+++
@@ -1,9 +0,0 @@
-def userEntity(item) -> dict :
-    return {
-        "id": str(item["_id"]),
-        "name": item["name"],
-        "password": item["password"]
-    }
-    
-def usersEntity(entity) -> list :
-    return [userEntity(item) for item in entity]
